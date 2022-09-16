17e0d30909d29df3e2f8bcd46c6903e2f32ed1d2
Fouad.Hijazi <100977191+FouadHijazi@users.noreply.github.com>
2022-09-16 05:09:20 +0300

    Add files via upload

--- Q2/scheam.sql
+++ Q2/scheam.sql
@@ -7,21 +7,21 @@ CREATE DATABASE Complexes;
  //**one to one
  and each building belongs to one complexs
  //**one to one 
-
-/* Requirement: */
+ 
+ /* Requirement: */
 /*- Write a SQL query to get a list of tenants who are renting more than one apartment */
-const query = `SELECT TenantID FROM AptTenants GROUP BY TenantsID HAVING count(*)>1) C ON  Tenants.TenantID=C.TenantID`
+const query = `SELECT TenantName FROM Tenants INNER JOIN (SELECT TenantID FROM AptTenants 
+ GROUP BY TenantsID HAVING count(*)>1 ) C ON  Tenants.TenantID=C.TenantID`
 
 /* - Write a SQL query to get a list of all buildings and the number of open requests (Requests in which
-status equals 'Open'). */
-
- const query_2 = `SELECT Apartments.BuildingID,count(*) as 'Count' FROM Requests INNER JOIN Apartments
+ status equals 'Open')*/
+const query_2 = `SELECT BuildingName, ISNULL(Count, 0) as'Count' FROM Buildings
+LEFT JOIN (SELECT Apartments.BuildingID,count(*) as 'Count' FROM Requests INNER JOIN Apartments 
 ON Requests.AptID=Apartments.AptID
 WHERE Requests.Status = 'Open'
 GROUP BY Apartments.BuildingID) ReqCounts
 ON ReqCounts.BuildingID=Buildings.BuildingID`
 
- /* - Building #11 is undergoing a major renovation. Implement a query to close all requests from
-apartments in this building. */
-
- const query_3 = `SELECT AptID FROM Apartments WHERE BuildingID=11`
+/* - Building #11 is undergoing a major renovation. Implement a query to close all requests from
+ apartments in this building. */
+const query_3 = `UPDATE Requests SET Status='Closed' WHERE AptID IN FROM Apartments WHERE BuildingID=11)`
